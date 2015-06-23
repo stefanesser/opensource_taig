1ff68aed174c48ab30dee63d1f83e0a9ad39692d
Stefan Esser <stefan.esser@sektioneins.de>
2015-06-24 00:04:42 +0200

    setup_watchdog_timer

--- untether.c
+++ untether.c
@@ -1,10 +1,24 @@
 #include <mach/mach.h>
 #include <mach/message.h>
+#include <IOKit/IOKitLib.h>
+#include <CoreFoundation/CoreFoundation.h>
 
 /* sub_c770 */
 mach_msg_return_t receive_mach_msg(mach_port_t rcv_name, mach_msg_header_t *msg, mach_msg_size_t rcv_size)
 {
 	return mach_msg(msg, MACH_RCV_MSG, 0, rcv_size, rcv_name, 0, 0);
+}
+
+/* sub_e568 */
+void setup_watchdog_timer(int value)
+{
+	io_service_t timerservice = IOServiceGetMatchingService(kIOMasterPortDefault, IOServiceMatching("IOWatchDogTimer"));
+	if (timerservice != 0) {
+		CFNumberRef cfval = CFNumberCreate(kCFAllocatorDefault, kCFNumberIntType, &value);
+		IORegistryEntrySetCFProperties(timerservice, cfval);
+		IOObjectRelease(timerservice);
+		CFRelease(cfval);
+	}
 }
 
 /* start */
